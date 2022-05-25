07fc7ccfb8a0943d0826bc857ab1176778f3cb00
karpss <ola.fam10@gmail.com>
2022-05-25 13:32:42 +0100

    json server for backend has been set up

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import React from 'react';
+import './App.css';
+
+const App: React.FunctionComponent = () => {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+};
+
+export default App;
